a0b9ce94e27ac048c8c96c4a5f95aed0d823ae32
AgamalARM <agamalarm@gmail.com>
2024-05-27 15:01:05 +0300

    add update

--- update.php
+++ update.php
@@ -14,6 +14,56 @@ if (!$user) {
     include 'partials/not_found.php';
     exit;
 }
-var_dump($user);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    updateUser($_POST,$userId);
+    if (isset($_FILES['picture'])) {
+        move_uploaded_file($_FILES['picture']["tmp_name"], "users/images/$userId.jpg");
+    }
+
+
+
+    //header("Location: index.php");
+}
+
+
 
 ?>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>Update User <b><?php echo $user['name'] ?></b></h3>
+        </div>
+            <div class="card-body">
+            <form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label>Name</label>
+        <input name="name" value="<?php echo $user['name']?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>User Name</label>
+        <input name="username" value="<?php echo $user['username']?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Email</label>
+        <input name="email" value="<?php echo $user['email']?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Phone</label>
+        <input name="phone" value="<?php echo $user['phone']?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Website</label>
+        <input name="website" value="<?php echo $user['website']?>" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Image</label>
+        <input name="picture" type="file" class="form-control-file">
+    </div>
+    
+    <button class="btn btn-success">Submit</button>
+
+         </form>
+        </div>
+    </div>
+ 
+</div>
